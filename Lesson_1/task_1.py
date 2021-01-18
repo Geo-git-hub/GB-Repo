d38ff32741f213714985f186fb5b6c29764a4468
Geo <georgiimiklaush@gmail.com>
2021-01-19 00:21:20 +0300

    Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
    Забежал немного вперёд, создал функцию.

--- Lesson_1/task_1.py
+++ Lesson_1/task_1.py
@@ -1,1 +1,14 @@
-print("Hello GitHub")
+def retur_format(num_to_format):
+    if len(str(num_to_format)) == 1:
+        num_to_format = "0" + str(num_to_format)
+    return num_to_format
+
+
+user_seconds = input("Укажите время в секундах: ")
+while not user_seconds.isdigit():
+    user_seconds = input("Укажите время в секундах (введите целое число): ")
+user_seconds = int(user_seconds)
+seconds = retur_format(user_seconds % 60)
+minutes = retur_format((user_seconds // 60) % 60)
+hours = retur_format((user_seconds // 60) // 60)
+print(f"\nВы ввели: {user_seconds} секунд, что составит {hours}:{minutes}:{seconds} ")
