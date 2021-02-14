eb9e807837c8df805c58ec446c7cc5e00da2eb7e
Geo <georgiy.miklaush@gmail.com>
2021-02-14 22:16:17 +0300

    Урок 8 задание 4, 5, 6: Изменил код 4 задачи с учётом задания 5 и 6 4) Начните работу над проектом «Склад оргтехники». Создайте класс, описывающий склад. А также класс «Оргтехника», который будет базовым для классов-наследников. Эти классы — конкретные типы оргтехники (принтер, сканер, ксерокс). В базовом классе определить параметры, общие для приведенных типов. В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники. ======================================================================== 5) Продолжить работу над первым заданием. Разработать методы, отвечающие за приём оргтехники на склад и передачу в определенное подразделение компании. Для хранения данных о наименовании и количестве единиц оргтехники, а также других данных, можно использовать любую подходящую структуру, например словарь. ======================================================================== 6) Продолжить работу над вторым заданием. Реализуйте механизм валидации вводимых пользователем данных. Например, для указания количества принтеров, отправленных на склад, нельзя использовать строковый тип данных.

--- Lesson_8/Task_8_4.py
+++ Lesson_8/Task_8_4.py
@@ -1,4 +1,5 @@
 """
+4)
 Начните работу над проектом «Склад оргтехники».
 Создайте класс, описывающий склад.
 А также класс «Оргтехника», который будет базовым для классов-наследников.
@@ -8,11 +9,69 @@
 """
 
 
+class NumberExcept(Exception):
+    def __init__(self, error_text):
+        self.error_text = error_text
+
+
 class Warehouse:
-    def __init__(self, name, address, capacity):
-        self.name = name
-        self.address = address
-        self.capacity = capacity
+    equipment_list = []
+
+    @staticmethod
+    def check_number(number):
+        try:
+            number = int(number)
+            if number < 0:
+                raise NumberExcept('Вы указали отрицательное число')
+        except ValueError as text:
+            return f'Некорректно указанны данные(введён текст): {text}'
+        except NumberExcept as text:
+            return f'{text}'
+        else:
+            return number
+
+    @classmethod
+    def get_equipment(cls, equipment):
+        list_to_check = ['quantity', 'price']
+
+        check_pass = True
+        for el in list_to_check:
+            if str(type(Warehouse.check_number(equipment.get(el)))) == "<class 'str'>":
+                check_pass = False
+                print(f'Для параметра: | {el} | в объекте {equipment} \n {Warehouse.check_number(equipment.get(el))}'
+                      f'\n ввод отменён.')
+
+        if check_pass:
+            Warehouse.equipment_list.append(equipment)
+
+    @classmethod
+    def receipt(cls, receipt_list):
+        for el in receipt_list:
+            Warehouse.get_equipment(el.__dict__)
+
+    @classmethod
+    def equipment_info(cls, type_equipment, value):
+        """Укажите наименование искомого ключа и искомое значение"""
+        info = f'На складе {type_equipment} - {value}: \n'
+        quantity = 0
+        for el in Warehouse.equipment_list:
+            if el.get(type_equipment) == value:
+                info += f' {el.get("name")}, код- {el.get("code")}  {el.get("quantity")} штук; \n'
+                quantity += el.get("quantity")
+
+        info += f' всего: {quantity} штук'
+        return info
+
+    @classmethod
+    def give_away(cls, code, quantity):
+        """Укажите код и передоваемое количество"""
+        for el in Warehouse.equipment_list:
+            if el.get('code') == code:
+                if el['quantity'] >= quantity:
+                    el['quantity'] = el['quantity'] - quantity
+                    print(f'Операция проведена успешно')
+                else:
+                    print(f'указанное количество больше доступного, операция отменена.')
 
 
 class OfficeEquipment:
@@ -55,13 +114,3 @@ class Xerox(OfficeEquipment):
     def __str__(self):
         return f'''{self.name, self.type_equipment, self.code, self.quantity, self.price, self.paper_size,
                     self.printing_technology, self.print_speed}'''
-
-
-scanner = Scanner('CanoScan LiDE 300', 'scanner', '2995C010', 100, 5600, '2400x2400 dpi')
-print(scanner)
-
-printer = Printer('ECOSYS P3155dn', 'printer', '1102TR3NL0', 32, 38000, 'A4', 'laser', 55)
-print(printer)
-
-xerox = Xerox('Xerox B215', 'xerox', 'B215DNI', 45, 20000, 'A4', 'laser', 40)
-print(xerox)
